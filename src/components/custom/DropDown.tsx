a01a73a7f0d9ddd6727d96a52899de9c2f1921cc
willie <jaysimwanza@gmail.com>
2022-01-11 19:36:41 +0200

    DropDrowLogic

---
+++ src/components/custom/DropDown.tsx
@@ -0,0 +1,69 @@
+import { Box, List, ListItem, ListItemButton, ListItemText, useTheme } from '@mui/material'
+import React, { useState } from 'react'
+import { COLORS } from '../../common/shared'
+import { Font } from '../font'
+
+interface Props {
+    calculators: string[] | undefined
+    func: any
+}
+
+const DropDown = (props: Props) => {
+    const {calculators} = props
+    const theme = useTheme();
+    const [selected, setSelected ] = useState<string>()
+    props.func(selected)
+   
+    return(
+        <Box sx={{
+            zIndex: 1,
+            position: 'absolute',
+            width: 300,
+            top: 55,
+            left: 10,
+            backgroundColor: theme.palette.background.paper,
+            boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
+            borderRadius: 2.5,
+            marginLeft: 2.5,
+          }}
+            className='text-center'
+          >
+            <List
+              className='text-center'
+              sx={{
+                color: COLORS.light_text_color,
+              }}
+            >
+              {calculators?.map((item: any, index) => (
+                <ListItem
+                  onClick={() => setSelected(item)}
+                  key={index}
+                  sx={{
+                    paddingBottom: 0.5,
+                    paddingTop: 0.5,
+                  }}
+                >
+                  <ListItemButton
+                    sx={{
+                      fontSize: 5,
+                      background: COLORS.gradient,
+                      padding: theme.spacing(0.5),
+                      borderRadius: '20px !important',
+                      marginBottom: 0,
+                      marginTop: 0,
+                    }}
+                  >
+                    <Font>
+                      <ListItemText
+                        primary={item}
+
+                      />
+                    </Font>
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+    )
+}
+export default DropDown
